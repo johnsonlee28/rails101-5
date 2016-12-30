d2ac17e9924f982c5ffba1848d602692b1c1e9b9
Johnsonlee <johnsonlee28@gmail.com>
2016-12-30 15:14:12 +0800

    add edit & delete function of myposts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    @group = Group.find(params[:group_id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -18,6 +23,26 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def update
+
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+      redirect_to account_posts_path
+  end
+
 
   private
 
